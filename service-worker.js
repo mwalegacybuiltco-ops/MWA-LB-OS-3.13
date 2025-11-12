d63531e975f653485a97558839a817a4b3a36e07
mwalegacybuiltco-ops <mwalegacybuiltco@gmail.com>
2025-11-11 17:00:46 -0700

    Update cache name in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Service Worker v12 */
-const CACHE='lbos-3-2-9-mobile-v12';
+const CACHE = 'lbos-3-2-9-mobile-v12a';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
